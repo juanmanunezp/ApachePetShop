7e4388d4edae7608171f087e7aa273e7f4f1b9dd
juanma <juanmanp13@gmail.com>
2024-09-23 23:52:41 -0300

    preEntregaUno

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
     <nav className="nav-wrapper white">
       <div className="container">
         <a href="/" className="brand-logo center">
-          <img src="../src/assets/logo.png" alt="logo" width={62} />
+          <img src="https://st.depositphotos.com/1217087/1591/v/950/depositphotos_15913957-stock-illustration-dog-and-leash.jpg" alt="logo" width={52} />
         </a>
         <ul id="nav-mobile" className="left hide-on-med-and-down">
           <li><a href="">PERRO</a></li>
@@ -17,7 +17,6 @@ const NavBar = () => {
         </ul>
         <div className="right cart-widget-container">
           <CartWidget />
-          
         </div>
       </div>
     </nav>
